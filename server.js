e359d24f94152add7ec54704525ec15b275d2c0b
natashabuck <natashabuck1@gmail.com>
2021-01-07 13:53:51 -0800

    added prod origin to list not sure if array is allowed

--- server.js
+++ server.js
@@ -9,7 +9,7 @@ const options = {
   origins: originList,
   handlePreflightRequest: ( req, res ) => {
     res.writeHead( 200, {
-      "Access-Control-Allow-Origin": "http://localhost:3000",
+      "Access-Control-Allow-Origin": originList,
       "Access-Control-Allow-Methods": "GET,POST",
       "Access-Control-Allow-Credentials": true
     } );
